eebfacbd685ae57e18b32f783802298ac2d9ec51
AmirHome <amir.email@yahoo.com>
2024-07-25 15:28:01 +0300

    release docker i

--- docker/release.sh
+++ docker/release.sh
@@ -17,14 +17,14 @@ $dc --env-file ./admin/.env up -d --build
 # wait for mysql to initialize
 sleep 5
 
-docker exec -t docker-lemp-php-fpm bash -c "chmod -R 775 storage"
-docker exec -t docker-lemp-php-fpm bash -c "chown -R www-data:www-data storage"
-docker exec -t docker-lemp-php-fpm bash -c "chmod -R 775 bootstrap/cache"
-docker exec -t docker-lemp-php-fpm bash -c "chown -R www-data:www-data bootstrap/cache"
+docker exec -i docker-lemp-php-fpm bash -c "chmod -R 775 storage"
+docker exec -i docker-lemp-php-fpm bash -c "chown -R www-data:www-data storage"
+docker exec -i docker-lemp-php-fpm bash -c "chmod -R 775 bootstrap/cache"
+docker exec -i docker-lemp-php-fpm bash -c "chown -R www-data:www-data bootstrap/cache"
 
-docker exec -t docker-lemp-php-fpm bash -c "php artisan optimize:clear"
-docker exec -t docker-lemp-php-fpm bash -c "php artisan storage:link"
-docker exec -t docker-lemp-php-fpm bash -c "composer update"
-docker exec -t docker-lemp-php-fpm bash -c "php artisan config:cache"
-docker exec -t docker-lemp-php-fpm bash -c "php artisan migrate:fresh --seed"
-docker exec -t docker-lemp-php-fpm bash -c "php artisan config:cache"
+docker exec -i docker-lemp-php-fpm bash -c "php artisan optimize:clear"
+docker exec -i docker-lemp-php-fpm bash -c "php artisan storage:link"
+docker exec -i docker-lemp-php-fpm bash -c "composer update"
+docker exec -i docker-lemp-php-fpm bash -c "php artisan config:cache"
+docker exec -i docker-lemp-php-fpm bash -c "php artisan migrate:fresh --seed"
+docker exec -i docker-lemp-php-fpm bash -c "php artisan config:cache"
