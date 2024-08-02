187100b19f803458d1ae82a09e2b7a54e0abb2b5
AmirHome <amir.email@yahoo.com>
2024-08-02 11:41:22 +0300

    release update II

--- docker/release.sh
+++ docker/release.sh
@@ -58,6 +58,10 @@ docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan config:cache"
 
 ### 9002
 docker exec -i docker-lemp-php-fpm-9002 bash -c "chown -R www-data:www-data ."
+docker exec -i docker-lemp-php-fpm-9002 bash -c "git config --global --add safe.directory /app2"
+docker exec -i docker-lemp-php-fpm-9002 bash -c "git reset --hard && git clean -df && git pull"
+
+
 docker exec -i docker-lemp-php-fpm-9002 bash -c "chmod -R 775 storage"
 # docker exec -i docker-lemp-php-fpm-9002 bash -c "chown -R www-data:www-data storage"
 docker exec -i docker-lemp-php-fpm-9002 bash -c "chmod -R 775 bootstrap/cache"
