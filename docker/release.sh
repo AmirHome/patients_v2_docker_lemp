44dc8b09d0f36dcc4db7bcb6c98015e976c7c854
AmirHome <amir.email@yahoo.com>
2024-08-08 17:12:29 +0300

    release

--- docker/release.sh
+++ docker/release.sh
@@ -15,6 +15,10 @@ for args in "$@"; do
     MIGRATESEED=true
     shift
     ;;
+  -mini)
+    MINI=true
+    shift
+    ;;
   *)
     echo "Invalid argument: $args"
     ;;
@@ -43,7 +47,7 @@ docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}
 #docker network ls
 docker network inspect docker_lemp_network
 
-### 9001
+### 9001 Admin app1
 docker exec -i docker-lemp-php-fpm-9001 bash -c "ping mysql -c 4"
 
 docker exec -i docker-lemp-php-fpm-9001 bash -c "chown -R www-data:www-data ."
@@ -58,31 +62,36 @@ docker exec -i docker-lemp-php-fpm-9001 bash -c "chmod -R 775 bootstrap/cache"
 docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan optimize:clear"
 docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan storage:link"
 docker exec -i docker-lemp-php-fpm-9001 bash -c "composer update"
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan key:generate"
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan config:cache"
+#docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan key:generate"
 
-### 9002
+docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan queue:work --daemon > /dev/null 2>&1"
+docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan queue:failed"
+
+docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan config:cache"
+docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan optimize"
+
+### 9002 Chat app2
 docker exec -i docker-lemp-php-fpm-9002 bash -c "chown -R www-data:www-data ."
 docker exec -i docker-lemp-php-fpm-9002 bash -c "git config --global --add safe.directory /app2"
 docker exec -i docker-lemp-php-fpm-9002 bash -c "git reset --hard && git clean -df && git pull"
 
 
 docker exec -i docker-lemp-php-fpm-9002 bash -c "chmod -R 775 storage"
-# docker exec -i docker-lemp-php-fpm-9002 bash -c "chown -R www-data:www-data storage"
 docker exec -i docker-lemp-php-fpm-9002 bash -c "chmod -R 775 bootstrap/cache"
+docker exec -i docker-lemp-php-fpm-9002 bash -c "php artisan link:storage"
+# docker exec -i docker-lemp-php-fpm-9002 bash -c "chown -R www-data:www-data storage"
 # docker exec -i docker-lemp-php-fpm-9002 bash -c "chown -R www-data:www-data bootstrap/cache"
 
 docker exec -i docker-lemp-php-fpm-9002 bash -c "php artisan optimize:clear"
-# docker exec -i docker-lemp-php-fpm-9002 bash -c "php artisan storage:link"
-#docker exec -i docker-lemp-php-fpm-9002 bash -c "composer update"
-docker exec -i docker-lemp-php-fpm-9002 bash -c "php artisan key:generate"
-docker exec -i docker-lemp-php-fpm-9002 bash -c "php artisan config:cache"
 
 # if argument seed is passed run this command
-# docker exec -it docker-lemp-php-fpm-9001 bash -c "php artisan migrate:fresh --seed"
 if [ "$MIGRATESEED" ]; then
     docker exec -it docker-lemp-php-fpm-9001 bash -c "php artisan migrate:fresh --seed"
-    docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan config:cache"
+else
+    docker exec -it docker-lemp-php-fpm-9001 bash -c "php artisan migrate --force"
 fi
+
+docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan config:cache"
+docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan optimize"
 
 docker ps
