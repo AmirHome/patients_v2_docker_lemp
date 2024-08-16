a79c002050eaffe1123399f48b6a54a2769c8817
AmirHome <amir.email@yahoo.com>
2024-08-16 12:22:35 +0300

    release

--- docker/release.sh
+++ docker/release.sh
@@ -20,7 +20,15 @@ if [ "$(whoami)" = "deploy" ]; then
   # ...
 else
   echo "This script must be run as the deploy user."
-  exit 1
+  #exit 1
+  # Are you sure you want to run this script as root?
+  # Ask for confirmation
+  if [ "$1" = "-y" ]; then
+    echo "Running as root"
+  else
+    echo "Please run this script as the deploy user."
+    exit 1
+  fi
 fi
 
 # Get all arguments
@@ -44,73 +52,84 @@ for args in "$@"; do
   esac
 done
 
+# Read the .env file
+if [ -f "symlink_app1/.env" ]; then
+  export $(cat symlink_app1/.env | grep -v '#' | awk '/=/ {print $1}')
+else
+  echo "File .env not found"
+  exit 1
+fi
 dc=$(which docker-compose)
 user=$(whoami)
 echo -e "### $dc \n"
 echo -e "### $user \n"
+echo -e "### ${APP_NAME} \n"
+
 
 # docker rm -f $(docker ps -a -q)
-docker rm -f docker-lemp-php-fpm-9001
-docker rm -f docker-lemp-php-fpm-9002
-docker rm -f docker-lemp-nginx
-docker rm -f docker-lemp-mysql
-docker rm -f docker-lemp-redis
+# docker rm -f docker-lemp-${APP_NAME}-php-fpm-9001
+# docker rm -f docker-lemp-${APP_NAME}-php-fpm-9002
+# docker rm -f docker-lemp-nginx
+# docker rm -f docker-lemp-mysql
+# docker rm -f docker-lemp-redis
 
-docker network prune -f
+# docker network prune -f
 
-$dc --env-file ./admin/.env up -d --build
+$dc down
+$dc --env-file ./symlink_app1/.env up -d --build
 
 # wait for mysql to initialize
-sleep 5
-docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' docker-lemp-mysql
+sleep 3
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' docker-lemp-${APP_NAME}-mysql
+
 #docker network ls
-docker network inspect docker_lemp_network
+# docker network inspect docker_lemp_network
 
 ### 9001 Admin app1 --------------------------------------------------------------------------------------------------
-docker exec -i docker-lemp-php-fpm-9001 bash -c "ping mysql -c 4"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "ping mysql -c 4"
 
-docker exec -i docker-lemp-php-fpm-9001 bash -c "chown -R www-data:www-data ."
-docker exec -i docker-lemp-php-fpm-9001 bash -c "git config --global --add safe.directory /app1"
-docker exec -it docker-lemp-php-fpm-9001 bash -c "git reset --hard && git clean -df && git pull"
+# docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chown -R www-data:www-data ."
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "git config --global --add safe.directory /app1"
+docker exec -it docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "git reset --hard && git clean -df && git pull"
 
-docker exec -i docker-lemp-php-fpm-9001 bash -c "chmod -R 775 storage"
-docker exec -i docker-lemp-php-fpm-9001 bash -c "chmod -R 775 bootstrap/cache"
-# docker exec -i docker-lemp-php-fpm-9001 bash -c "chown -R www-data:www-data storage"
-# docker exec -i docker-lemp-php-fpm-9001 bash -c "chown -R www-data:www-data bootstrap/cache"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chmod -R 775 storage"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chmod -R 775 bootstrap/cache"
+# docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chown -R www-data:www-data storage"
+# docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chown -R www-data:www-data bootstrap/cache"
 
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan optimize:clear"
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan storage:link"
-docker exec -i docker-lemp-php-fpm-9001 bash -c "composer update"
-#docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan key:generate"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan optimize:clear"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan storage:link"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "composer update"
+#docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan key:generate"
 
-docker exec -i docker-lemp-php-fpm-9001 bash -c "nohup php artisan queue:work --daemon >> storage/logs/laravel.log &"
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan queue:failed"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "nohup php artisan queue:work --daemon >> storage/logs/laravel.log &"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan queue:failed"
 
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan config:cache"
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan optimize"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan config:cache"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan optimize"
 
 ### 9002 Chat app2 --------------------------------------------------------------------------------------------------
-docker exec -i docker-lemp-php-fpm-9002 bash -c "chown -R www-data:www-data ."
-docker exec -i docker-lemp-php-fpm-9002 bash -c "git config --global --add safe.directory /app2"
-docker exec -i docker-lemp-php-fpm-9002 bash -c "git reset --hard && git clean -df && git pull"
+# docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "chown -R www-data:www-data ."
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "git config --global --add safe.directory /app2"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "git reset --hard && git clean -df && git pull"
 
 
-docker exec -i docker-lemp-php-fpm-9002 bash -c "chmod -R 775 storage"
-docker exec -i docker-lemp-php-fpm-9002 bash -c "chmod -R 775 bootstrap/cache"
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan storage:link"
-# docker exec -i docker-lemp-php-fpm-9002 bash -c "chown -R www-data:www-data storage"
-# docker exec -i docker-lemp-php-fpm-9002 bash -c "chown -R www-data:www-data bootstrap/cache"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "chmod -R 775 storage"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "chmod -R 775 bootstrap/cache"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "php artisan storage:link"
+# docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "chown -R www-data:www-data storage"
+# docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "chown -R www-data:www-data bootstrap/cache"
 
-docker exec -i docker-lemp-php-fpm-9002 bash -c "php artisan optimize:clear"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "php artisan optimize:clear"
 
 # if argument seed is passed run this command
 if [ "$MIGRATESEED" ]; then
-    docker exec -it docker-lemp-php-fpm-9001 bash -c "php artisan migrate:fresh --seed"
+    docker exec -it docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan migrate:fresh --seed"
 else
-    docker exec -it docker-lemp-php-fpm-9001 bash -c "php artisan migrate --force"
+    docker exec -it docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan migrate --force"
 fi
 
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan config:cache"
-docker exec -i docker-lemp-php-fpm-9001 bash -c "php artisan optimize"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan config:cache"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan optimize"
 
 docker ps
