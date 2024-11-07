72a48c6bfb7cefdb6c04730a3bcb8fc640a64987
AmirHome <amir.email@yahoo.com>
2024-11-07 18:53:08 +0300

    Release III for sync files

--- docker/release.sh
+++ docker/release.sh
@@ -19,11 +19,35 @@ if [ "$(whoami)" = "deploy" ]; then
   # Command 2
   git reset --hard && git clean -fd && git pull
 
+  # Sync uploads folder
+  sudo chown -R deploy:deploy /home/deploy/patientv2/admin/storage/app/public/uploads/
+  rsync -avzh patientsclinics@160.153.244.148:/home/patientsclinics/public_html/patient_code/public/uploads/ /home/deploy/patientv2/admin/storage/app/public/uploads
+  ## ]&E{?d{{djl1
+
+  ## docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chown -R www-data:www-data ."
+
   # Existing commands in release.sh
   # ...
 else
   echo "This script must be run as the deploy user."
-  # exit 1
+
+  exit 1
+  # get path of this script
+  SCRIPT=$(readlink -f "$0")
+  SCRIPTPATH=$(dirname "$SCRIPT")
+
+  # Stop all containers and remove all containers
+  docker stop $(docker ps -a -q)
+  docker rm $(docker ps -a -q)
+  # Remove all images
+  # docker rmi $(docker images -q)
+  # Remove all volumes and networks
+  docker volume prune -f
+  docker network prune -f
+  
+  rm -rf $SCRIPTPATH/mysql/data/*
+  rm -rf $SCRIPTPATH/mysql/logs/*
+  
 
 fi
 
@@ -53,8 +77,9 @@ for args in "$@"; do
 done
 
 # Read the .env file
-if [ -f "symlink_app1/.env" ]; then
-  export $(cat symlink_app1/.env | grep -v '#' | awk '/=/ {print $1}')
+if [ -f "symlink_app1/.env.admin" ]; then
+  export $(cat symlink_app1/.env.admin | grep -v '#' | awk '/=/ {print $1}')
+  sudo cp symlink_app1/.env.admin symlink_app1/.env
 
 else
   echo "File .env not found"
@@ -95,23 +120,16 @@ if [ "$(whoami)" = "deploy" ]; then
   docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chown -R www-data:www-data ."
   docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "git config --global --add safe.directory /app1"
   docker exec -it docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "git reset --hard origin/master && git clean -df && git pull"
-  #php artisan media-library:clear
-  #php artisan media-library:regenerate --only-missing
+
 fi
 
 docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chmod -R 775 storage"
 docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chmod -R 775 bootstrap/cache"
-### docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chown -R www-data:www-data storage"
-### docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "chown -R www-data:www-data bootstrap/cache"
 
 docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "composer dump-autoload"
 docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan optimize:clear"
 docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan storage:link"
 docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "composer update"
-docker exec -it docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan media-library:regenerate --only-missing"
-
-docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "nohup php artisan queue:work --daemon >> storage/logs/laravel.log &"
-docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan queue:failed"
 
 ### 9002 Chat app2 --------------------------------------------------------------------------------------------------
 if [ "$(whoami)" = "deploy" ]; then
@@ -128,6 +146,9 @@ docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "chmod -R 775 bootst
 docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "php artisan optimize:clear"
 docker exec -i docker-lemp-${APP_NAME}-php-fpm-9002 bash -c "php artisan storage:link"
 
+### Test to connect to mysql
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan migrate:status"
+
 # if argument seed is passed run this command
 if [ "$MIGRATESEED" ]; then
   docker exec -it docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan migrate:fresh --seed"
@@ -135,6 +156,14 @@ else
   docker exec -it docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan migrate --force"
 fi
 
+### After migrate we need to run queue and regenerate media library
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "nohup php artisan queue:work --daemon >> storage/logs/laravel.log &"
+docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan queue:failed"
+docker exec -it docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan media-library:regenerate --only-missing"
+# php artisan media-library:clear
+# php artisan media-library:regenerate --only-missing
+
+### Don't cache config
 if [ "$(whoami)" = "deploy" ]; then
   docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan config:cache"
   docker exec -i docker-lemp-${APP_NAME}-php-fpm-9001 bash -c "php artisan optimize"
